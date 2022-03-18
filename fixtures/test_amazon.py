eb2182069a4757ff1d2ed7a546dbc28d78b78e7d
Саша Филиппов <apple@MacBook-Pro.local>
2022-03-18 10:32:12 +0300

    add base

--- fixtures/test_amazon.py
+++ fixtures/test_amazon.py
@@ -1,7 +1,16 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
+
+    def test_homepage(self):
+        driver = webdriver.Chrome()
+        driver.find_element()
 
 
     def test_homepage(self):
